b3a3b9d41203148346a35985fc7425446f6de22a
Agboola Yusuf <agboolar09@gmail.com>
2018-10-11 15:16:39 +0100

    initial commit

---
+++ LoggerService/Contracts/ILoggerManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LoggerService.Contracts
+{
+    public interface ILoggerManager
+    {
+        void LogInfo(string message);
+        void warn(string message);
+        void debug(string message);
+        void error(string message);
+    }
+}
---
+++ LoggerService/LoggerManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LoggerService.Contracts;
+
+namespace LoggerService
+{
+    public class LoggerManager: ILoggerManager
+    {
+
+        public void debug(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogInfo(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
